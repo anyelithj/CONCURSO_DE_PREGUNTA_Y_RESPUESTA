35a0118906ca0478010d1e032dfcac603186830d
Anyelith Jacobo <anyelithj@misena.edu.co>
2022-06-14 13:35:03 -0500

    change name function

--- preguntas-server/src/main.ts
+++ preguntas-server/src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function init() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(process.env.PORT || 3650);
+  // await app.listen(process.env.PORT || 3650);
+  await app.listen( process.env.PORT || 3650, ()=>{
+    console.log("Iniciando")
+  });
 }
-bootstrap();
+init();
+
+
